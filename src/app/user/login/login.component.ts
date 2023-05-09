af9ec00875c0f4fe6c1053e6f4b12be25ed0d34e
nika tsitsilashvili <56062872+02nika@users.noreply.github.com>
2023-05-09 22:15:35 +0400

    login component changed

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+
+  loginForm = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
+
+  showAlert = false;
+  alertMsg = 'Please wait! Checking your account.';
+  alertColor = 'blue';
+
+  OnLogin() {
+    console.log(this.email, 'email');
+    console.log(this.password, 'password');
+
+    this.showAlert = true;
   }
-
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -4,36 +4,41 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-    name = new FormControl('',[Validators.required,Validators.minLength(3)])
-    email= new FormControl('',[Validators.required,Validators.email])
-    age = new FormControl('',[Validators.required,Validators.min(18),Validators.max(120)])
-    password = new FormControl('',[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)])
-    confirm_password = new FormControl('',[Validators.required])
-    phoneNumber = new FormControl('',[Validators.required,Validators.minLength(9),Validators.maxLength(9)])
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+  confirm_password = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(9),
+    Validators.maxLength(9),
+  ]);
 
-  showAlert=false
-  alertMsg='Please wait! Your account is being created.'
-  alertColor='blue'
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
 
   registerForm = new FormGroup({
     name: this.name,
     email: this.email,
-    age:this.age,
-    password:this.password,
-    cofirm_password:this.confirm_password,
-    phoneNumber:this.phoneNumber
-  })
+    age: this.age,
+    password: this.password,
+    cofirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
+  });
 
-  register(){
+  register() {
     this.showAlert = true;
-    this.alertMsg= 'Please wait! Your account is being created.'
-    this.alertColor='blue'
   }
-
-
-
 }
- 
